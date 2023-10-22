bc8c047168f4d2fd2393763917e0a29d260c0a23
baramundi666 <mkrol@student.agh.edu.pl>
2023-10-22 19:59:48 +0200

    minor final changes

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -81,8 +81,4 @@ public class Vector2d {
     }
 
 
-
-
-
-
 }
--- oolab/src/test/java/agh/ics/oop/model/Vector2dTest.java
+++ oolab/src/test/java/agh/ics/oop/model/Vector2dTest.java
@@ -10,6 +10,22 @@ public class Vector2dTest {
 
 
     @Test
+    public void testEquals() {
+        int x = 2;
+        int y = 2;
+
+
+        var v1 = new Vector2d(x, y);
+        var v2 = new Vector2d(x, y);
+        var v3 = new Vector2d(x+3, y-3);
+
+
+        assertTrue(v1.equals(v2));
+        assertFalse(v1.equals(v3));
+    }
+
+
+    @Test
     public void testToString() {
         int x = 2;
         int y = 3;
@@ -35,10 +51,31 @@ public class Vector2dTest {
     @Test
     public void testFollows() {
         var other = new Vector2d(3, 2);
-        var vector = new Vector2d(other.getx()+1, other.gety()+2);
+        var vector = new Vector2d(other.getx()+1, other.gety());
 
 
         assertTrue(vector.follows(other));
+    }
+
+
+    @Test
+    public void testUpperRight() {
+        var v1 = new Vector2d(3, 2);
+        var v2 = new Vector2d(1, 5);
+        var v3 = new Vector2d(3, 5);
+
+
+        assertEquals(v1.upperRight(v2), v3);
+    }
+
+    @Test
+    public void testLowerLeft() {
+        var v1 = new Vector2d(3, 2);
+        var v2 = new Vector2d(1, 5);
+        var v3 = new Vector2d(1, 2);
+
+
+        assertEquals(v1.lowerLeft(v2), v3);
     }
 
 
@@ -65,49 +102,13 @@ public class Vector2dTest {
 
 
     @Test
-    public void testUpperRight() {
-        var v1 = new Vector2d(3, 2);
-        var v2 = new Vector2d(1, 5);
-        var v3 = new Vector2d(max(3, 1), max(2, 5));
-
-
-        assertEquals(v1.upperRight(v2), v3);
-    }
-
-    @Test
-    public void testLowerLeft() {
-        var v1 = new Vector2d(3, 2);
-        var v2 = new Vector2d(1, 5);
-        var v3 = new Vector2d(min(3, 1), min(2, 5));
-
-
-        assertEquals(v1.lowerLeft(v2), v3);
-    }
-
-
-    @Test
     public void testOpposite() {
-        var v1 = new Vector2d(3, 2);
-        var v2 = new Vector2d(-3, -2);
+        var v1 = new Vector2d(3, -2);
+        var v2 = new Vector2d(-3, 2);
 
 
         assertEquals(v1.opposite(), v2);
     }
 
-
-    @Test
-    public void testEquals() {
-        int x = 2;
-        int y = 2;
-
-
-        var v1 = new Vector2d(x, y);
-        var v2 = new Vector2d(x, y);
-        var v3 = new Vector2d(x+3, y-3);
-
-
-        assertTrue(v1.equals(v2));
-        assertFalse(v1.equals(v3));
-    }
 
 }
