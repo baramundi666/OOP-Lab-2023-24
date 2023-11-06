6c7f9ed52e41cecff240f3ef9079a487cd5b6b6a
Mateusz Król <mateuszkrol221@gmail.com>
2023-11-06 14:12:49 +0100

    change to simulation class

--- oolab/src/main/java/agh/ics/oop/Simulation.java
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -14,13 +14,11 @@ public class Simulation {
     private final WorldMap<Animal, Vector2d> map;
     private final List<MoveDirection> directions;
 
-    private final List<Vector2d> positions;
     private final List<Animal> animals;
 
     Simulation (List<MoveDirection> directions, List<Vector2d> positions, WorldMap<Animal, Vector2d> map) {
         this.map = map;
         this.directions = directions;
-        this.positions = positions;
         this.animals = new LinkedList<>();
         for (Vector2d position: positions) {
             var animal = new Animal(position);
@@ -31,10 +29,6 @@ public class Simulation {
 
     List<MoveDirection> getDirections() {
         return new LinkedList<>(directions);
-    }
-
-    List<Vector2d> getPositions() {
-        return new LinkedList<>(positions);
     }
 
     List<Animal> getAnimals() {
